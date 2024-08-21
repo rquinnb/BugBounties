adb8ff6ef227a5d0113484f66a677af9d340ce76
rquinnb <89413994+rquinnb@users.noreply.github.com>
2024-08-21 00:52:08 -0500

    Update phishing_sim.js

--- phishing_sim.js
+++ phishing_sim.js
@@ -1,4 +1,4 @@
 document.body.innerHTML = `
   <h1>Phishing Simulation</h1>
-  <p>Please <a href="https://www.example.com">click here</a> to win a prize!</p>
+  <p>Please <a href="https://raw.githubusercontent.com/rquinnb/BugBounties/main/HARMLESS.js">click here</a> to win a prize!</p>
 `;
